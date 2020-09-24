f8f3c2dd7f36b07d3d681db91bd6fdc559e6b2fe
igor <gob1in@Mail.ru>
2020-09-24 12:59:53 +0300

    added files from old prj

---
+++ src/by/it/khmel/jd01_05/TaskA.java
@@ -0,0 +1,21 @@
+package by.it.khmel.jd01_05;
+
+
+
+
+public class TaskA {
+    public static void main(String[] args) {
+
+        step1();
+
+    }
+
+    private static void step1() {
+        double x=0.3;
+        double a=756.13;
+
+
+        }
+
+
+}
