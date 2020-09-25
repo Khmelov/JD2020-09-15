dd940cabc0eb31e9d4603be2879f334f625c5426
Evgeniya1595 <zhenya3361595>
2020-09-25 22:37:55 +0300

    jd01_01

--- src/by/it/khmel/jd01_05/TaskB.java
+++ src/by/it/khmel/jd01_05/TaskB.java
@@ -1,5 +1,54 @@
 package by.it.khmel.jd01_05;
 
-public class TaskB {
+import static java.lang.Math.*;
 
+public class TaskB {
+    public static void main(String[] args) {
+
+        step1();
+        step2();
+    }
+
+
+    private static void step1() {
+        double y = 0;
+        double a = 0;
+        for (a = 0; a <= 2; a += 0.2) {
+            for (int x = 1; x <= 6; x++) {
+                y = y + pow(7, a) - cos(x);
+            }
+            System.out.printf("при a= %4.1f  y=%6f", a, y);
+        }
+    }
+
+    private static void step2() {
+        double x=-6;
+        double b, a;
+
+        while (x>=-6 &&x <2) {
+
+            if ((x / 2) > -2 && (x / 2) <= -1) {
+                b = sin(x * x);
+                a = log10(abs(b + 2.74));
+                System.out.printf("при x/2= %5.2f a = %f", (x / 2), a);
+            }
+            if ((x / 2) > -1 && (x / 2) < 0.2) {
+                b = cos(x * x);
+                a = log10(abs(b + 2.74));
+                System.out.printf("при x/2= %5.2f a = %f", (x / 2), a);
+
+            }
+            if ((x / 2) == 0.2) {
+                b = 1 / tan(x * x);
+
+                a = log10(abs(b + 2.74));
+                System.out.printf("при x/2= %5.2f a = %f", (x / 2), a);
+
+            }
+                x = x + 0.5;
+        }
+    }
 }
+
+
+
---
+++ src/by/it/khmel/jd01_05/TaskC.java
@@ -0,0 +1,79 @@
+package by.it.khmel.jd01_05;
+
+import java.util.Random;
+
+import static java.lang.Math.*;
+
+public class TaskC {
+    public static void main(String[] args) {
+
+        double[] array;
+        array = step1();
+        step2(array);
+        double[] arrayres = step3(array);
+        step2(arrayres);
+        step4(arrayres);
+        System.out.println(step4(arrayres));
+
+    }
+
+    private static double[] step1() {
+
+        int n = (int) (random() * 20 + 40);
+
+        //  System.out.println(n);
+        double[] array = new double[n];
+        double x, z;
+        int i = 0;
+        for (x = 5.33; x <= 9; x = x + (9 - 5.33) / n) {
+            if (i < n) {
+                z = pow((x * x + 4.5), 1 / 3.0);
+                //System.out.println(z);
+                array[i] = z;
+                i = i + 1;
+            }
+        }
+        return array;
+    }
+
+    private static void step2(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("x[%d]=%5.3f  ", i, array[i]);
+            System.out.println();
+        }
+
+    }
+
+    private static double[] step3(double[] array) {
+        int m = 0;
+        int j = 0;
+
+        // double[] arrayres=new double[8];
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] < 3.5) {
+                m = m + 1;
+            }
+        }
+        double[] resarray = new double[m];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 3.5) {
+                resarray[j] = array[i];
+                j = j + 1;
+            }
+
+        }
+        return resarray;
+    }
+
+
+    private static double step4(double []array) {
+        double avergeom=1;
+        for (int i = 0; i < array.length; i++) {
+            avergeom=avergeom*array[i];
+        }
+        avergeom=pow(avergeom,1/(array.length*1.0));
+        return avergeom;
+    }
+}
