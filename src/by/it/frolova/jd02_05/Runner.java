92f619fe4509bdf05888b0bcb2db3c7cbdb784dc
jmabracodabra <jmabracodabra@gmail.com>
2020-10-30 18:18:11 +0300

    Added jd02_05 TaskB

--- src/by/it/frolova/jd02_05/Runner.java
+++ src/by/it/frolova/jd02_05/Runner.java
@@ -1,20 +1,53 @@
 package by.it.frolova.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
         Lang manager = Lang.INSTANCE;
+        Scanner sc = new Scanner(System.in);
+        Date date = new Date();
+        Locale locale = new Locale("en", "US");
         if (args.length == 2) {
-            Locale locale = new Locale(args[0], args[1]);
+            locale = new Locale(args[0], args[1]);
             manager.setLocale(locale);
+            print(manager);
+            printDate(date, locale);
         } else {
-            manager.setLocale(Locale.ENGLISH);
-        }
+            for (; ; ) {
+                String inputLocale = sc.nextLine();
+                if (inputLocale.equals("end")) {
+                    break;
+                } else if (inputLocale.equals("en")) {
+                    locale = new Locale("en", "US");
+                    manager.setLocale(locale);
+                } else if (inputLocale.equals("be")) {
+                    locale = new Locale("be", "BY");
+                    manager.setLocale(locale);
+                } else if (inputLocale.equals("ru")) {
+                    locale = new Locale("ru", "RU");
+                    manager.setLocale(locale);
+                } else {
+                    manager.setLocale(Locale.ENGLISH);
+                }
 
-        System.out.println(manager.get(Greeting.HELLO));
-        System.out.println(manager.get(Greeting.QUESTION));
-        System.out.println(manager.get(User.FIRST_NAME));
-        System.out.println(manager.get(User.LAST_NAME));
+                print(manager);
+                printDate(date, locale);
+            }
+        }
+    }
+
+    private static void printDate(Date date, Locale locale) {
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, locale);
+        System.out.println(dateFormat.format(date));
+    }
+
+    private static void print(Lang manager) {
+        System.out.printf("%s\n%s\n%s\n%s\n", manager.get(Greeting.HELLO)
+                , manager.get(Greeting.QUESTION), manager.get(User.FIRST_NAME)
+                , manager.get(User.LAST_NAME));
     }
 }
