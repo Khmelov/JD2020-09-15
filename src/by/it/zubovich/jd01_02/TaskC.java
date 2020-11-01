a06c87b35c94b2f55a6fd3c4f22b1c018a2c1697
maer1 <tutbora@gmail.com>
2020-11-01 17:55:13 +0300

    I finished jd02_01 To be continue...

--- src/by/it/zubovich/jd01_02/TaskC.java
+++ src/by/it/zubovich/jd01_02/TaskC.java
@@ -10,7 +10,7 @@ public class TaskC {
         Scanner sc = new Scanner(System.in);
         int[][] arr = step1(sc.nextInt());
         System.out.println(Arrays.deepToString(arr));
-        step2(arr);
+        //step2(arr);
         step3(arr);
     }
     private static int[][] step1(int n) {
@@ -35,24 +35,103 @@ public class TaskC {
         while (!(minOk || maxOk));
         return arr;
     }
-    private static int step2(int[ ][ ] arr) {
+
+/*    private static int step2(int[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            int temp = 0;
-            for (int i1 = 0; i1 < arr[i].length - 1; i1++) {
-                if (arr[i][i1] > 0) {
-                    temp += 1;
+
+            for (int j = 0; j < arr[i].length; j++) { //TODO
+                int tempPositive = 0;
+                int tempNegative = 0;
+                for (int k = 1; k < arr[i].length - 1; k++) {
+                    if (arr[i][j] > 0 && arr[i][k] < 0 && arr[i][k] > 0) {
+                        sum = sum + arr[i][j];
+                    }
                 }
-                if (arr[i][i1 + 1] < 0 && temp == 1) {
-                    sum += arr[i][i1 + 1];
+                *//*if (arr[i][j+1]<0){
+                    tempNegative = tempNegative + 1;
                 }
+
+                if (arr[i][j + 1] < 0 && tempPositive == 1 && arr[i][j + 2] > 0) {
+                    sum = sum + arr[i][j + 1];
+                }*//*
             }
         }
-        System.out.println("\n" + sum);
         return sum;
+    }*/
+//        System.out.println("\n" + sum + "\n");
+//        return sum;
+
+
+        private static int[ ][ ] step3(int[ ][ ] arr){
+            int max = arr[0].length;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (max < arr[i][j]) {
+                        max = arr[i][j];
+                    }
+                }
+            }
+            System.out.println(max);
+
+            boolean[] line = new boolean[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == max) {
+                        line[i] = true;
+                    }
+                }
+            }
+            //System.out.println(Arrays.toString(line));
+
+            boolean[] vertLine = new boolean[arr.length];
+            for (int i = 0; i < arr[0].length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[j][i] == max) {
+                        vertLine[i] = true;
+                    }
+                }
+            }
+            //System.out.println(Arrays.toString(vertLine));
+
+            int lines = 0;
+            int vertLines = 0;
+            for (int i = 0; i < line.length; i++) {
+                if (!line[i]) {
+                    lines++;
+                }
+            }
+            for (int i = 0; i < vertLine.length; i++) {
+                if (!vertLine[i]) {
+                    vertLines++;
+                }
+            }
+            System.out.println("line " + lines + ", top " + vertLines);
+
+            int[][] arrFinal = new int[lines][vertLines];
+
+            int iN = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (!line[i]) {
+                    int jN = 0;
+                    for (int j = 0; j < arr[i].length; j++) {
+                        if (!vertLine[j]) {
+                            arrFinal[iN][jN] = arr[i][j];
+                            jN++;
+                        }
+                    }
+                    iN++;
+                }
+
+            }
+
+            for (int i = 0; i < arrFinal.length; i++) {
+                System.out.println();
+                for (int i1 = 0; i1 < arrFinal[i].length; i1++) {
+                    System.out.print(arrFinal[i][i1] + " ");
+                }
+            }
+            return arrFinal;
+        }
     }
-    private static int[ ][ ] step3(int[ ][ ] arr){
-        
-        return arr;
-    }
-}
+
---
+++ src/by/it/zubovich/jd02_01/Manager.java
@@ -0,0 +1,5 @@
+package by.it.zubovich.jd02_01;
+class Manager {
+    static int buyersInMarket;
+    static final int K_SPEED = 100;
+}
