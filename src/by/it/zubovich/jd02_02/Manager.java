abc47bc755071369101f717a3dbf31238cae58e6
maer1 <tutbora@gmail.com>
2020-11-02 13:18:47 +0300

    I finished jd02_01A jd02_02A To be continue...

---
+++ src/by/it/zubovich/jd02_02/Manager.java
@@ -0,0 +1,33 @@
+package by.it.zubovich.jd02_02;
+
+class Manager {
+    private Manager() {
+
+    }
+
+    static final int K_SPEED = 100;
+    private static volatile int buyersEnterToMarket = 0;
+    private static volatile int buyersLeftMarket = 0;
+    private static final int buyerTotal = 100;
+
+    static synchronized void addBuyer() {
+
+        buyersEnterToMarket++;
+    }
+
+    static void leftBuyer() {
+        synchronized (Manager.class) {
+            buyersLeftMarket++;
+        }
+    }
+
+    static boolean marketIsOpened() {
+
+        return buyersEnterToMarket != buyerTotal;
+    }
+
+    static boolean marketIsClosed() {
+
+        return buyersLeftMarket == buyerTotal;
+    }
+}
