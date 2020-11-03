d12807e62bbc67fdbd577dec6b40a0c0270bf7d1
maer1 <tutbora@gmail.com>
2020-11-03 15:09:49 +0300

    I finished jd_01_03ABC jd02_01A jd02_02A jd02_03A jd02_05A To be continue...

--- src/by/it/zubovich/jd01_03/Helper.java
+++ src/by/it/zubovich/jd01_03/Helper.java
@@ -2,19 +2,19 @@ package by.it.zubovich.jd01_03;
 
 public class Helper {
 
-    static void sort(double[ ] massive) {
+    static void sort(double[] massive) {
         for (int last = massive.length - 1; last >= 0; last--) {
             for (int i = 0; i < last; i++) {
-                if (massive[i]>massive[i+1]){
-                    double tmp=massive[i];
-                    massive[i]=massive[i+1];
-                    massive[i+1]=tmp;
+                if (massive[i] > massive[i + 1]) {
+                    double tmp = massive[i];
+                    massive[i] = massive[i + 1];
+                    massive[i + 1] = tmp;
                 }
             }
         }
     }
 
-    static double findMax(double[ ] arr) {
+    static double findMax(double[] arr) {
         if (0 == arr.length) {
             return Integer.MAX_VALUE;
         } else {
@@ -41,8 +41,9 @@ public class Helper {
             return min;
         }
     }
-    static double[ ] mul(double[ ][ ] matrix, double[ ] vector){
-        double[ ] arr = new double[matrix.length];
+
+    static double[] mul(double[][] matrix, double[] vector) {
+        double[] arr = new double[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             for (int i1 = 0; i1 < vector.length; i1++) {
                 arr[i] = arr[i] + matrix[i][i1] * vector[i1];
@@ -50,12 +51,13 @@ public class Helper {
         }
         return arr;
     }
-    static double[ ][ ] mul(double[ ][ ] matrixLeft, double[ ][ ] matrixRight){
-        double[ ][ ] arr = new double[matrixLeft.length][matrixRight[0].length];
+
+    static double[][] mul(double[][] matrixLeft, double[][] matrixRight) {
+        double[][] arr = new double[matrixLeft.length][matrixRight[0].length];
         for (int i = 0; i < matrixLeft.length; i++) {
             for (int j = 0; j < matrixRight[0].length; j++) {
                 for (int k = 0; k < matrixRight.length; k++) {
-                    arr [i][j] = arr [i][j] + matrixLeft[i][k] * matrixRight[k][j];
+                    arr[i][j] = arr[i][j] + matrixLeft[i][k] * matrixRight[k][j];
                 }
             }
         }
--- src/by/it/zubovich/jd01_03/InOut.java
+++ src/by/it/zubovich/jd01_03/InOut.java
@@ -2,16 +2,16 @@ package by.it.zubovich.jd01_03;
 
 /**
  * @author R.Zubovich
- *
  */
 
 class InOut {
     /**
      * Метд преобразование строки в массив
+     *
      * @param line - Входная строка (с числами)
      * @return Возвращаемый массив
      */
-    static double[] getArray(String line){
+    static double[] getArray(String line) {
         //1 2 3 4 5 6 7 8
         String[] strArr = line.split(" ");
         double[] res = new double[strArr.length];
@@ -20,17 +20,19 @@ class InOut {
         }
         return res;
     }
-    static void printArray(double[ ] arr){
+
+    static void printArray(double[] arr) {
         for (double element : arr) {
             System.out.println(element + " ");
         }
         System.out.println();
     }
-    static void printArray(double[ ] arr, String name, int columnCount){
+
+    static void printArray(double[] arr, String name, int columnCount) {
         for (int i = 0; i < arr.length; i++) {
             //V[1] = 12.1234
             System.out.printf("%s[% -3d]=%10.4f ", name, i, arr[i]);
-            if ((i + 1)%columnCount == 0 || i +1 == arr.length){
+            if ((i + 1) % columnCount == 0 || i + 1 == arr.length) {
                 System.out.println();
             }
         }
